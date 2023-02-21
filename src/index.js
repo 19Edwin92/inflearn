5e648b217a00b4e311fc2d26206cffca8d6428e5
19Edwin92 <b_a_s_e@naver.com>
2023-02-21 15:46:28 +0900

    ver04

--- src/index.js
+++ src/index.js
@@ -1,16 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Library from './chapter_03/Library';
+// import Library from './chapter_03/Library';
+// import Clock from './chapter_04/Clock';
+import Commentlist from './chapter_05/Commentlist';
 
+//chapter05 댓글 만들기
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Library />
+    <Commentlist />
   </React.StrictMode>
 );
+
+
+
+
+//chapter04 - clock
+// setInterval(()=> {}, 1000) 1초마다 화면이 새롭게 덧씌어지도록 했다. 
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// setInterval(() => {
+//   root.render(
+//     <React.StrictMode>
+//       <Clock />
+//     </React.StrictMode>
+//   );
+// })
+
+//chapter03
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <Library />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
